161c35afadf5a171148b23e6e14f8d9b478bef02
mdtalhachy <talhachowdhury3.1416@gmail.com>
2020-07-03 18:35:26 +0600

    blue.flavor_ updated details, added colors to tabs

--- app/src/main/java/rocks/talha/portfoliodetalha/MainActivity.java
+++ app/src/main/java/rocks/talha/portfoliodetalha/MainActivity.java
@@ -1,6 +1,7 @@
 package rocks.talha.portfoliodetalha;
 
-;import android.support.design.widget.TabLayout;
+;import android.graphics.Color;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -21,5 +22,7 @@ public class MainActivity extends AppCompatActivity {
 
         TabLayout tabLayout = findViewById(R.id.tabLayout);
         tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.setTabTextColors(Color.GRAY, Color.parseColor("white"));
     }
 }
